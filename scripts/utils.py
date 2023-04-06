c1518b3587af74114c429466b3610f32f990201d
TasbasMN <nazifts@gmail.com>
2023-04-06 11:20:39 +0300

    upload changes

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,15 +1,24 @@
-from collections import defaultdict
-from itertools import pairwise, combinations
-
-from Bio import SeqIO
-import pandas as pd
-
 import subprocess
 import uuid
 import os
 import contextlib
 import math
 
+from collections import defaultdict
+from itertools import pairwise, combinations
+
+from Bio import SeqIO
+import pandas as pd
+
+from tools import *
+
+# ----------------------------------------------------------------------------------------------------------------------
+# pyensembl stuff
+from pyensembl import EnsemblRelease
+# set pyensembl cache location
+os.environ['PYENSEMBL_CACHE_DIR'] = "../../data"
+
+# ----------------------------------------------------------------------------------------------------------------------
 
 def find_matches(sequence, mirna_df, ignore_first_15_nucleotides=True, find_6mers=False, comparison_columns=False):
     # sourcery skip: low-code-quality
@@ -35,7 +44,7 @@ def find_matches(sequence, mirna_df, ignore_first_15_nucleotides=True, find_6mer
         seed_dict[db_seeds[i]].append((db_names[i], db_sequences[i]))
 
     # step 2: creating sequence generator
-    generator = sliding_window(dna_to_mrna(sequence), 8)
+    generator = sliding_window(thymine_to_uracil(sequence), 8)
 
     # step 3: initializing lists
     names = []
@@ -135,18 +144,19 @@ def find_matches(sequence, mirna_df, ignore_first_15_nucleotides=True, find_6mer
 
 
 def import_fasta(fasta):
-    """imports fasta into a string
+    """imports fasta into a string, returns mRNA
+
 
     Args:
         fasta (file): fasta file
 
     Returns:
-        str: sequence string
+        str: sequence string, mRNA
     """
 
     with open(fasta) as f:
         records = SeqIO.parse(f, "fasta")
-        transcripts = [dna_to_mrna(str(rec.seq)) for rec in records]
+        transcripts = [thymine_to_uracil(str(rec.seq)) for rec in records]
         return "".join(transcripts)
 
 
@@ -160,8 +170,8 @@ def sliding_window(sequence, win_size):
 ########################################################################################################################
 # string manipulation functions
 
-def complement(string):
-    """computes the complement of a nucleic acid sequence
+def rna_complement(string):
+    """computes the RNA complement of a nucleic acid sequence
     """
     result = ""
     for nuc in string:
@@ -174,24 +184,6 @@ def complement(string):
         elif nuc in ["U", "T"]:
             result += "A"
     return result
-
-
-def uracil_to_thymine(string):
-    """changes uracils into thymines
-    """
-    return "".join("T" if nuc == "U" else nuc for nuc in string)
-
-
-def mirna_to_mrna(string):
-    """converts a miRNA string into a mRNA string
-    """
-    return complement(string[::-1])
-
-
-def dna_to_mrna(string):
-    """converts a dna string into a mrna string
-    """
-    return "".join("U" if nuc == "T" else nuc for nuc in string)
 
 
 def get_dna_seq_from_coordinates(sequence, start, end):
@@ -223,6 +215,7 @@ def pretty_print(df, summary=True):
 
 def print_results(results_df, sequence, summary=True):
     """prints match results in TargetScan style
+    
 
     Args:
         df (dataframe): df containing find_matches() results
@@ -231,9 +224,11 @@ def print_results(results_df, sequence, summary=True):
     Returns:
         dataframe: df containing multiline strings in TargetScan style
     """
-
-    names, match_types, mirna_sequences, starts, ends = unpack_results_df(
-        results_df)
+    names = results_df.name.tolist()
+    mirna_sequences = results_df.mirna_sequence.tolist()
+    match_types = results_df.match_type.tolist()
+    starts = results_df.start.tolist()
+    ends = results_df.end.tolist()
 
     result_strings = []
 
@@ -250,7 +245,7 @@ def print_results(results_df, sequence, summary=True):
         # creating sequence strings
         dna_string = f"5' {uracil_to_thymine(sequence_slice)} 3' position: {dna_start+1}-{dna_end} of DNA"
         mirna_string = "3' " + whitespace_length * \
-            (" ") + complement(mirna_sequences[i]) + " 5' " + names[i]
+            (" ") + rna_complement(mirna_sequences[i]) + " 5' " + names[i]
 
         # creating middle string
         match_string = ""
@@ -278,20 +273,6 @@ def print_results(results_df, sequence, summary=True):
 ########################################################################################################################
 # feature column generator functions
 
-def unpack_results_df(results_df):
-    """unpacks results dataframe in a tidy way
-
-    Args:
-        results_df (pd.DataFrame): results of find_matches()
-    """
-    names = results_df.name.tolist()
-    mirna_sequences = results_df.mirna_sequence.tolist()
-    match_types = results_df.match_type.tolist()
-    starts = results_df.start.tolist()
-    ends = results_df.end.tolist()
-
-    return names, match_types, mirna_sequences, starts, ends
-
 
 def generate_3utr_length_column(sequence, results_df):
     """
@@ -317,8 +298,11 @@ def generate_3_supplementary_pairing_column(sequence, results_df, extended=False
         pd.DataFrame: result dataframe with an extra column
     """
 
-    names, match_types, mirna_sequences, starts, ends = unpack_results_df(
-        results_df)
+    names = results_df.name.tolist()
+    mirna_sequences = results_df.mirna_sequence.tolist()
+    match_types = results_df.match_type.tolist()
+    starts = results_df.start.tolist()
+    ends = results_df.end.tolist()
 
     results = []
 
@@ -484,9 +468,11 @@ def generate_close_proximity_column(results_df):
 
 def generate_total_no_of_pairs_column(sequence, results_df):
 
-    names, match_types, mirna_sequences, starts, ends = unpack_results_df(
-        results_df)
-
+    names = results_df.name.tolist()
+    mirna_sequences = results_df.mirna_sequence.tolist()
+    match_types = results_df.match_type.tolist()
+    starts = results_df.start.tolist()
+    ends = results_df.end.tolist()
     results = []
 
     for i, _ in enumerate(names):
@@ -537,14 +523,13 @@ def generate_position_in_utr_column(sequence, results_df):
 
 def generate_flanking_dinucleotides_columns(sequence, results_df):
 
-    names = results_df["name"].values.tolist()
     starts = results_df["start"].values.tolist()
     ends = results_df["end"].values.tolist()
 
     results_a5, results_u5, results_g5, results_c5, results_a3, results_u3, results_g3, results_c3 = ([
     ] for _ in range(8))
 
-    for i, _ in enumerate(names):
+    for i, _ in enumerate(starts):
 
         a5 = u5 = g5 = c5 = a3 = u3 = g3 = c3 = 0
 
@@ -599,7 +584,6 @@ def generate_flanking_dinucleotides_columns(sequence, results_df):
 
 def generate_local_au_content_column(sequence, results_df):
 
-    names = results_df["name"].values.tolist()
     starts = results_df["start"].values.tolist()
     ends = results_df["end"].values.tolist()
 
@@ -611,7 +595,7 @@ def generate_local_au_content_column(sequence, results_df):
     final_scores_3end = []
 
     # for each miRNA;
-    for i, _ in enumerate(names):
+    for i, _ in enumerate(starts):
 
         start_5end = (starts[i]-30) if starts[i] > 30 else 1
         end_5end = starts[i]-1
@@ -660,7 +644,6 @@ def generate_ta_column(results_df, ta_sps_df):
 def generate_sps_column(results_df, ta_sps_df):
 
     # unpacking results df
-    names = results_df["name"].values.tolist()
     seed_without_anchor_as = results_df["seed_without_anchor_a"].values.tolist(
     )
     match_types = results_df["match_type"].values.tolist()
@@ -680,7 +663,7 @@ def generate_sps_column(results_df, ta_sps_df):
 
     results = [0 for _ in range(len(results_df))]
 
-    for i, _ in enumerate(names):
+    for i, _ in enumerate(match_types):
 
         if match_types[i] in ["6mer", "7mer-A1"] and seed_without_anchor_as[i] in seeds_to_6mer_sps_dict:
             results[i] = bartel_6mer_sps[i]
@@ -720,6 +703,9 @@ def one_hot_encode_match_types(results_df):
     results_df["a1"] = a1
 
     return results_df
+
+########################################################################################################################
+# ViennaRNA stuff
 
 
 def get_pair_probabilities_of_sequence(sequence, delete_temp_files=True, window_size=80, bp_range=40, length_of_region=16, log10_output=True):
@@ -771,14 +757,14 @@ def get_pair_probabilities_of_sequence(sequence, delete_temp_files=True, window_
     log10_results = []
     for i in unpaired_probabilities:
         if i == "NA":
-            log10_results.append(0)
+            log10_results.append("NaN")
         else:
             log10_results.append(round(math.log10(float(i)), 3))
 
     return log10_results
 
 
-def get_accessibility_column(sequence, results_df, log10_results=True):
+def generate_accessibility_column(sequence, results_df, log10_results=True):
 
     ends = results_df["end"].values.tolist()
 
@@ -796,3 +782,254 @@ def get_accessibility_column(sequence, results_df, log10_results=True):
     results_df["accessibility"] = results
 
     return results_df
+
+
+def invoke_rnaduplex(mrna_sequence, mirna_sequence, energy_range=5.0, rnaduplex_location="/usr/local/bin/RNAduplex"):
+
+    # this step takes reverse complement of the mirna sequence (which was in mRNA form)
+    mirna_sequence_5p = rna_complement(mirna_sequence[::-1])
+
+    input_sequence = mirna_sequence_5p + "\n" + mrna_sequence
+    input_sequence = input_sequence.encode()
+
+    # run RNAduplex
+    rnaduplex_subprocess = subprocess.Popen(
+        [rnaduplex_location, "-e", f"{energy_range}", "-s"],
+        stdout=subprocess.PIPE,
+        stdin=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+    output, error = rnaduplex_subprocess.communicate(input=input_sequence)
+    rnaduplex_subprocess.wait()
+
+    return output.decode()
+
+
+def generate_rnaduplex_energy_column(sequence, results_df, energy_range=5.0):
+
+    mirna_sequences = results_df["mirna_sequence"].values.tolist()
+    starts = results_df["start"].values.tolist()
+    ends = results_df["end"].values.tolist()
+
+    results = []
+
+    for i, _ in enumerate(mirna_sequences):
+        mrna_sequence = sequence[starts[i]-1:ends[i]-1]
+        mirna_sequence = mirna_sequences[i]
+
+        output = invoke_rnaduplex(
+            mrna_sequence, mirna_sequence, energy_range=energy_range)
+
+        # splitting lines
+        temp = [line for line in output.split("\n") if len(line) != 0]
+
+        # splitting result string into elements
+        temp = temp[0].split()
+
+        # temp[-1] contains energy value with parantheses
+        energy = float(temp[-1].strip("()"))
+
+        results.append(energy)
+
+    results_df["rnaduplex_energy"] = results
+    return results_df
+
+
+def generate_stable_duplex_column(sequence, results_df, energy_cutoff=-10.0, energy_range=5):
+    """surveys the sequence for stable binding sites using a free energy cutoff. For each miRNA seed sequence in results, this function invokes RNAduplex to find duplex sites with high stability.
+
+    Args:
+        sequence (str): Sequence of interest
+        results_df (pd.DataFrame): pandas dataframe containing find_matches() results
+        energy_cutoff (float): Duplex free energy cutoff. Energy values higher than this cutoff are discarded.
+        energy_range (float): Compute suboptimal structures with energy in a certain range of the optimum (kcal/mol)
+    """
+
+    seeds = results_df["seed_match"].values.tolist()
+    starts = results_df["start"].values.tolist()
+
+    results = []
+
+    for i, _ in enumerate(seeds):
+
+        output = invoke_rnaduplex(
+            sequence, seeds[i], energy_range=energy_range)
+
+        # splitting lines
+        temp = [line for line in output.split("\n") if len(line) != 0]
+        # splitting result string into elements
+        temp = temp[0].split()
+
+        coordinates = temp[3].split(",")
+
+        # +1 is there because of the differences between our & rnaduplex notation
+        # in rnaduplex output, coordinates are given with indices not included
+        # our start coordinates are included, so we need to add 1
+        found_start = int(coordinates[0])+1
+
+        energy = float(temp[-1].strip("()"))
+
+        if energy < energy_cutoff and found_start == starts[i]:
+            results.append(1)
+        else:
+            results.append(0)
+
+    results_df["stable_duplex"] = results
+
+    return results_df
+
+
+def invoke_rnacofold(mrna_sequence, mirna_sequence, rnacofold_location="/usr/local/bin/RNAcofold"):
+    """invokes RNAduplex to calculate secondary structures of two RNAs with dimerization
+    """
+
+    # this step takes reverse complement of the mirna sequence (which was in mRNA form)
+    mirna_sequence_5p = rna_complement(mirna_sequence[::-1])
+
+    input_sequence = f"{mirna_sequence_5p}&{mrna_sequence}"
+    input_sequence = input_sequence.encode()
+
+    # run RNAcofold
+    # d1 limits the energy contribution of dangling ends
+    # --output-format=D makes the output in csv format
+    # --csv-noheader removes csv headers as they're not necessary
+
+    rnacofold_subprocess = subprocess.Popen(
+        [rnacofold_location, "-d1", "--output-format=D", "--csv-noheader", "--noPS"],
+        stdout=subprocess.PIPE,
+        stdin=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+    output, error = rnacofold_subprocess.communicate(input=input_sequence)
+    rnacofold_subprocess.wait()
+    return output.decode()
+
+
+def generate_rnacofold_energy_column(sequence, results_df):
+
+    mirna_sequences = results_df["mirna_sequence"].values.tolist()
+    starts = results_df["start"].values.tolist()
+    ends = results_df["end"].values.tolist()
+
+    results = []
+
+    for i, _ in enumerate(mirna_sequences):
+        mrna_sequence = sequence[starts[i]-1:ends[i]-1]
+        mirna_sequence = mirna_sequences[i]
+
+        output = invoke_rnacofold(mrna_sequence, mirna_sequence)
+
+        energy = float(output.split(",")[-1])
+
+        results.append(energy)
+
+    results_df["rnacofold_energy"] = results
+    return results_df
+
+
+def run_script(sequence):
+    """runs the script on input sequence string
+
+    Args:
+        sequence (str): sequence of interest
+    """
+    
+    # loading data dfs
+    targetscan_df = pd.read_csv("../data/supplementary_files/targetscan.tsv", sep="\t")
+    ta_sps_df = pd.read_csv("../data/supplementary_files/ta_sps.tsv", sep="\t")
+    
+    
+    results_df = find_matches(sequence, targetscan_df)
+    generate_ta_column(results_df, ta_sps_df)
+    generate_sps_column(results_df, ta_sps_df)
+    generate_3utr_length_column(sequence, results_df)
+    generate_3_supplementary_pairing_column(sequence, results_df)
+    generate_3_supplementary_pairing_column(sequence, results_df, extended=True)
+    generate_avg_position_column(results_df)
+    generate_close_proximity_column(results_df)
+    generate_total_no_of_pairs_column(sequence, results_df)
+    generate_position_in_utr_column(sequence, results_df)
+    generate_flanking_dinucleotides_columns(sequence, results_df)
+    generate_local_au_content_column(sequence, results_df)
+    one_hot_encode_match_types(results_df)
+    generate_stable_duplex_column(sequence, results_df, energy_cutoff=-20.0, energy_range=5)
+    generate_accessibility_column(sequence, results_df)
+    generate_rnacofold_energy_column(sequence, results_df)
+    generate_rnaduplex_energy_column(sequence, results_df)
+    
+    return results_df
+    
+    
+    # ----------------------------------------------------------------------------------------------------------------------
+    
+def preprocess_clash_data(data="../data/supplementary_files/clash_data_parsed.tsv", drop_irrelevant_columns=True):
+    """
+    Preprocess the parsed CLASH data and return a df
+    """
+    clash_df = pd.read_csv(data, sep="\t", header=2)
+
+    if drop_irrelevant_columns:
+        columns_to_keep = ["microRNA_name", "miRNA_seq", "mRNA_name", "mRNA_start", "mRNA_end_extended", "mRNA_seq_extended", "seed_type"]
+        clash_df = clash_df[columns_to_keep]
+    # unzipping columns
+    mirnas = clash_df["microRNA_name"].tolist()
+    mirna_accessions = []
+    mirna_names = []
+
+    for i in mirnas:
+        temp = i.split("_")
+        mirna_accessions.append(temp[0])
+        mirna_names.append(temp[2])
+
+    clash_df["mirna_accession"] = mirna_accessions
+    clash_df["mirna_name"] = mirna_names
+    clash_df.drop(["microRNA_name"], axis=1, inplace=True)
+
+    # --------------------------------------------------
+    mrnas = clash_df["mRNA_name"].values.tolist()
+    mrna_ensg_ids = []
+    mrna_enst_ids = []
+    mrna_names = []
+
+    for i in mrnas:
+        temp = i.split("_")
+        mrna_ensg_ids.append(temp[0])
+        mrna_enst_ids.append(temp[1])
+        mrna_names.append(temp[2])
+
+    clash_df["mrna_enst_id"] = mrna_enst_ids
+    clash_df["mrna_ensg_id"] = mrna_ensg_ids
+    clash_df["mrna_name"] = mrna_names
+    clash_df.drop(["mRNA_name"], axis=1, inplace=True)
+
+    return clash_df
+
+
+def get_ensembl60_data(clash_df):
+
+    # set pyensembl
+    os.environ['PYENSEMBL_CACHE_DIR'] = "../../data"
+    data = EnsemblRelease(60)
+    data.download()
+    data.index()
+    
+    ensts = clash_df["mrna_enst_id"].values.tolist()
+    
+    contigs = []
+    starts = []
+    ends = []
+    
+    for i in ensts:
+        obj = data.transcript_by_id(i)
+
+        contigs.append(obj.contig)
+        starts.append(obj.start)
+        ends.append(obj.end)
+
+    clash_df["ensembl60_contig"] = contigs
+    clash_df["ensembl60_start"] = starts
+    clash_df["ensembl60_end"] = ends
+    
+    return clash_df
